a3b2ccaf99ac5b4c9800dc820043ea56821d198e
YoungGwon Kim <97ykkim@naver.com>
2022-05-03 00:21:02 +0900

    코드변경 : MainPopularItem.js 코드 변경

--- src/components/MainPopularItem.js
+++ src/components/MainPopularItem.js
@@ -3,13 +3,17 @@ import React from "react";
 function MainPopularItem({shoes, idx}) {
     return (
         <>
-            <div className="col-md-4 populars">
-                <img src={`https://raw.githubusercontent.com/younggwons/younggwons.github.io/master/item/shoes${idx}.jpg`} width="80%" height="190px" />
-                <h4>{shoes.title}</h4>
-                <p>{shoes.content}</p>
-                <p>{shoes.price}</p>
-            </div>
-            
+            {
+                idx < 21 
+                ? (
+                    <div className="col-md-4 populars">
+                        <img src={`https://raw.githubusercontent.com/younggwons/younggwons.github.io/master/item/shoes${idx}.jpg`} width="80%" height="190px" />
+                        <h4>{shoes.title}</h4>
+                        <p>{shoes.content}</p>
+                        <p>{shoes.price}</p>
+                    </div>
+                ) : null
+            }
         </>        
     )
 }
