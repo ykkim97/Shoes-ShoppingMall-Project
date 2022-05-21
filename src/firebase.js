aec8fb7427920e2e78bdb7a6a42f6bdd53b0074b
YoungGwon Kim <97ykkim@naver.com>
2022-05-21 23:57:09 +0900

    필요없는 comment 제거

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCCwt8AVPXoEGpX2XHzoBR6SYLJsRuUOak",
     authDomain: "shoes-shoppingmall.firebaseapp.com",
@@ -16,7 +12,6 @@ const firebaseConfig = {
     appId: "1:533905534329:web:6d37786f454f9edec753a8"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase();
 
