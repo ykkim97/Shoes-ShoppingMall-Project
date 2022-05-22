60b6200ea9c0dc8024d4e6dc21856157b6203713
YoungGwon Kim <97ykkim@naver.com>
2022-05-22 21:30:31 +0900

    Suspense 코드 정리

--- src/App.js
+++ src/App.js
@@ -41,126 +41,112 @@ function App() {
     <div className="App">
       <Router>
         <ScrollToTop />
-        <Routes>
-          
-          {/* 메인 페이지 */}
-          <Route exact path='/' element={
-            <Home 
-              popularShoes={popularShoes}
-              isLogged={isLogged}
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            {/* 메인 페이지 */}
+            <Route exact path='/' element={
+              <Home 
+                popularShoes={popularShoes}
+                isLogged={isLogged}
+                setIsLogged={setIsLogged}
+              />
+            }></Route>
+
+            {/* 로그인 페이지 */}
+            <Route exact path='/auth' element={
+                <Auth 
+                  user={user} 
+                  setUser={setUser}
+                  isLogged={isLogged}
+                  setIsLogged={setIsLogged}
+                />
+            }></Route>
+
+            {/* 회원가입 페이지 */}
+            <Route exact path='/auth/signup' element={
+                <SignUp />
+            }></Route>
+
+            {/* 제품 별 상세페이지 */}
+            <Route exact path='/detail/:id' element={
+                <Detail 
+                  popularShoes={popularShoes} 
+                  setPopularShoes={setPopularShoes}
+                  isLogged={isLogged}
+                  setIsLogged={setIsLogged}
+                />
+            }></Route>
+
+            {/* 남성신발 페이지 */}
+            <Route exact path='/man' element={
+                <ManShoes 
+                  isLogged={isLogged}
+                  setIsLogged={setIsLogged}
+                  popularShoes={popularShoes}
+                  setPopularShoes={setPopularShoes}
+                  ax={ax}
+                />
+            }></Route>
+
+            {/* 여성신발 페이지 */}
+            <Route exact path='/woman' element={
+                <WomanShoes 
+                  isLogged={isLogged}
+                  setIsLogged={setIsLogged}
+                  popularShoes={popularShoes}
+                  setPopularShoes={setPopularShoes}
+                  ax={ax}
+                />
+            }></Route>
+
+            {/* 키즈신발 페이지 */}
+            <Route exact path='/kids' element={
+                <KidsShoes 
+                  isLogged={isLogged}
+                  setIsLogged={setIsLogged}
+                  popularShoes={popularShoes}
+                  setPopularShoes={setPopularShoes}
+                  ax={ax}
+                />
+            }></Route>
+
+            {/* 장바구니 페이지 */}
+            <Route exact path='/cart' element={
+                <Cart 
+                  isLogged={isLogged} 
+                  setIsLogged={setIsLogged}
+                />
+            }></Route>
+
+            {/* 마이 페이지 */}
+            <Route exact path='/mypage' element={
+                <MyPage isLogged={isLogged} setIsLogged={setIsLogged}/>
+            }></Route>
+
+            {/* 최근 본 상품 */}
+            <Route exact path='/watched' element={
+                <Watched 
+                  isLogged={isLogged} 
+                  setIsLogged={setIsLogged}
+                  popularShoes={popularShoes}
+                />
+            }></Route>
+
+            {/* DB 테스트용 */}
+            <Route exact path='/test' element={<Test 
+              isLogged={isLogged} 
               setIsLogged={setIsLogged}
-            />
-          }></Route>
+              popularShoes={popularShoes}
+            />}></Route>
 
-          {/* 로그인 페이지 */}
-          <Route exact path='/auth' element={
-            <Suspense fallback={<Loading />}>
-              <Auth 
-                user={user} 
-                setUser={setUser}
-                isLogged={isLogged}
-                setIsLogged={setIsLogged}
-              />
-            </Suspense>
-          }></Route>
-
-          {/* 회원가입 페이지 */}
-          <Route exact path='/auth/signup' element={
-            <Suspense fallback={<Loading />}>
-              <SignUp />
-            </Suspense>
-          }></Route>
-
-          {/* 제품 별 상세페이지 */}
-          <Route exact path='/detail/:id' element={
-            <Suspense fallback={<Loading />}>
-              <Detail 
-                popularShoes={popularShoes} 
-                setPopularShoes={setPopularShoes}
-                isLogged={isLogged}
-                setIsLogged={setIsLogged}
-              />
-            </Suspense>
-          }></Route>
-
-          {/* 남성신발 페이지 */}
-          <Route exact path='/man' element={
-            <Suspense fallback={<Loading />}>
-              <ManShoes 
-                isLogged={isLogged}
-                setIsLogged={setIsLogged}
-                popularShoes={popularShoes}
-                setPopularShoes={setPopularShoes}
-                ax={ax}
-              />
-            </Suspense>
-          }></Route>
-
-          {/* 여성신발 페이지 */}
-          <Route exact path='/woman' element={
-            <Suspense fallback={<Loading />}>
-              <WomanShoes 
-                isLogged={isLogged}
-                setIsLogged={setIsLogged}
-                popularShoes={popularShoes}
-                setPopularShoes={setPopularShoes}
-                ax={ax}
-              />
-            </Suspense>
-          }></Route>
-
-          {/* 키즈신발 페이지 */}
-          <Route exact path='/kids' element={
-            <Suspense fallback={<Loading />}>
-              <KidsShoes 
-                isLogged={isLogged}
-                setIsLogged={setIsLogged}
-                popularShoes={popularShoes}
-                setPopularShoes={setPopularShoes}
-                ax={ax}
-              />
-            </Suspense>
-          }></Route>
-
-          {/* 장바구니 페이지 */}
-          <Route exact path='/cart' element={
-            <Suspense fallback={<Loading />}>
-              <Cart 
-                isLogged={isLogged} 
-                setIsLogged={setIsLogged}
-              />
-            </Suspense>
-          }></Route>
-
-          {/* 마이 페이지 */}
-          <Route exact path='/mypage' element={
-            <Suspense fallback={<Loading />}>
-              <MyPage isLogged={isLogged} setIsLogged={setIsLogged}/>
-            </Suspense>
-          }></Route>
-
-          {/* 최근 본 상품 */}
-          <Route exact path='/watched' element={
-            <Suspense fallback={<Loading />}>
-              <Watched 
-                isLogged={isLogged} 
-                setIsLogged={setIsLogged}
-                popularShoes={popularShoes}
-              />
-            </Suspense>
-          }></Route>
-
-          {/* DB 테스트용 */}
-          <Route exact path='/test' element={<Test 
-            isLogged={isLogged} 
-            setIsLogged={setIsLogged}
-            popularShoes={popularShoes}
-          />}></Route>
-
-          {/* 그 외 404에러 */}
-          <Route path='*' element={<div><img src='/img/404error.jpg'></img></div>}></Route>
-
-        </Routes>
+            {/* 그 외 404에러 */}
+            <Route path='*' element={
+              <div>
+                <img src='/img/404error.jpg'></img>
+              </div>
+            }></Route>
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
