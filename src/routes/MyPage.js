05d74d53b698cea3c2bb19338ad4e35ccab943aa
YoungGwon Kim <97ykkim@naver.com>
2022-05-22 23:28:29 +0900

    MyPage 스타일 수정

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -6,22 +6,23 @@ import { useNavigate } from "react-router-dom";
 
 // styled-components
 const Bar = styled.div`
-    background : #cfcccc;
+    background : rgb(233, 227, 227);
     width : 100%;
     max-width : 1000px;
     height : ${props => props.maxWidth};;
-    border-top : 3px solid black;
-    border-bottom : 3px solid black;
+    border-right : 3px solid rgb(192, 181, 181);
+    border-bottom : 3px solid rgb(192, 181, 181);
+    border-radius : 5px;
     color : black;
     display : block;
     margin : auto;
-    margin-bottom : 30px;
+    margin-bottom : 70px;
 `
 
 const Item = styled.li`
     list-style : none;
-    padding-top : 20px;
-    padding-right : 20px;
+    padding-top : 5px;
+    padding-right : 25px;
     font-size : 20px;
     cursor: pointer;
 `
@@ -36,20 +37,33 @@ function MyPage({isLogged, setIsLogged}) {
             <Bar maxWidth={"150px"}>
                 <h5 className={styles.orderCheckTitle}>주문/배송조회</h5>
                 <ul className={styles.orderCheckItem}>
-                    <Item>입금/결제</Item>
-                    <Item>배송중</Item>
-                    <Item>배송완료</Item>
-                    <Item>구매확정</Item>
-                    <Item>환불교환</Item>
+                    <Item className={styles.item}>입금/결제</Item>
+                    <Item className={styles.item}>배송중</Item>
+                    <Item className={styles.item}>배송완료</Item>
+                    <Item className={styles.item}>구매확정</Item>
+                    <Item className={styles.item}>환불교환</Item>
                 </ul>
             </Bar>
-            <Bar maxWidth={"600px"}>
+            <Bar maxWidth={"150px"}>
                 <h5 className={styles.myShoppingTitle}>나의 쇼핑 활동</h5>
-                <ul>
-                    <Item onClick={() => {navigate("/cart")}}>장바구니</Item>
-                    <Item>주문내역조회</Item>
-                    <Item>1:1 문의</Item>
-                    <Item onClick={() => {navigate("/watched")}}>최근 본 상품</Item>
+                <ul className={styles.myShoppingItem}>
+                    <Item 
+                        onClick={() => {navigate("/cart")}}
+                        className={styles.item}
+                    >장바구니</Item>
+                    <Item className={styles.item}>주문내역조회</Item>
+                    <Item className={styles.item}>1:1 문의</Item>
+                    <Item 
+                        onClick={() => {navigate("/watched")}}
+                        className={styles.item}
+                    >최근 본 상품</Item>
+                </ul>
+            </Bar>
+            <Bar maxWidth={"150px"}>
+                <h5 className={styles.userInfo}>회원 정보</h5>
+                <ul className={styles.userInfoItem}>
+                    <Item className={styles.item}>계정정보 확인</Item>
+                    <Item className={styles.item}>비밀번호 재설정</Item>
                 </ul>
             </Bar>
         </>
